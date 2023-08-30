0387d55c488f68f2eff2dd782372741587f25821
Gazi2050 <gazi45344@gmail.com>
2023-08-31 04:10:59 +0600

    fourth commit

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-const loadPhone = async () => {
-    const res = await fetch(' https://openapi.programming-hero.com/api/phones?search=iphone')
+
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json()
     const phones = data.data
     displayPhones(phones)
@@ -9,6 +10,8 @@ const loadPhone = async () => {
 const displayPhones = phones => {
     const phoneContainer = document.getElementById('phone-container')
 
+    phoneContainer.textContent = '';
+    phones = phones.slice(0, 12)
     phones.forEach(phone => {
         console.log(phone)
         const phoneCard = document.createElement('div');
@@ -17,7 +20,7 @@ const displayPhones = phones => {
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <p>$999</p>
             <div class="card-actions justify-end">
                 <button class="btn btn-primary">Buy Now</button>
             </div>
@@ -28,9 +31,9 @@ const displayPhones = phones => {
 }
 
 const handleSearch = () => {
-    const searchField = document.getElementById('search-field')
-
-
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText)
 }
 
-loadPhone()
+// loadPhone()
