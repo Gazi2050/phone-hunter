de7fa6fbd6aabf205dd4ce8f823c0a2b958ce718
Gazi2050 <gazi45344@gmail.com>
2023-08-31 02:03:26 +0600

    third commit

--- test.js
+++ test.js
@@ -7,9 +7,30 @@ const loadPhone = async () => {
 }
 
 const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phone-container')
+
     phones.forEach(phone => {
         console.log(phone)
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-50 shadow-xl p-2`
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `
+        phoneContainer.appendChild(phoneCard)
     });
+}
+
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field')
+
+
 }
 
 loadPhone()
